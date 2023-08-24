6c8434045c31bedf35d06f2a25407097e477fc8f
Connor Keyes <connorkeyes@Connors-MacBook-Air.local>
2023-08-24 14:32:04 -0700

    snail chases your mouse and tries to kill you, crashes youtube if achieved

--- main.js
+++ main.js
@@ -2,72 +2,83 @@ class Snail {
     constructor(el) {
         this.node = document.createElement('img');
         this.node.setAttribute('src', 'https://img.freepik.com/premium-vector/vector-cute-snail-black-white-coloring_75487-1585.jpg?w=2000');
-        this.node.setAttribute('id', 'snail');
-        this.node.style.zIndex = 10000;
-        this.node.style.borderRadius = '5px';
-        // this.speed = 500;
+        const coordinates = generateStartingPosition();
         this.node.style.height = '50px';
-        this.node.style.width = '50px';  
-        this.node.style.top = '0px';
-        this.node.style.left = '0px';
-        // this.node.style.color = 'pink';
+        this.node.style.width = '50px';
+        this.node.style.position = 'absolute';
+        this.node.style.top = `${coordinates[0]}px`;
+        this.node.style.left = `${coordinates[1]}px`;
+        this.node.style.zIndex = 100000;
+        this.node.style.borderRadius = '6px';
 
-        el.appendChild(this.node);
-        // console.log(this);
+        this.targetPosition = { x: 0, y: 0 };
     }
-    
-    // move() {
 
-    //     let leftPosition = 0;
-    //     console.log('aaaaaaaaaaaaaaAAAAAAAAAAAAAAA')
-    //     // console.log(leftPosition + 100);
-        
-        
-    //     leftPosition += 200000;
-    //     this.node.style.left = `${leftPosition}px`;
-    //     console.log(leftPosition);
-    //     console.log(`${leftPosition}`);
-    //     console.log(`${leftPosition}px`);
-    //     console.log(leftPosition); // --> 100
-    //     //this.node.style.left = String(leftPosition) + 'px';
-    //     //this.node.style.top = String(topPosition) + 'px';
-        
-    //     console.log(this.node.style.left)
-    //     //setTimeout(this.move(), this.speed);
-    // }
+    move() {
+        const leftPosition = Number(this.node.style.left.replace('px', ''));
+        const topPosition = Number(this.node.style.top.replace('px', ''));
+
+        if (leftPosition > this.targetPosition.x - 80) {
+            this.node.style.left = `${leftPosition - 1}px`;
+            this.node.style.transform = 'scaleX(1)';
+        } else {
+            this.node.style.left = `${leftPosition + 1}px`;
+            this.node.style.transform = 'scaleX(-1)';
+        }
+
+        if (topPosition > this.targetPosition.y - 20) {
+            this.node.style.top = `${topPosition - 1}px`;
+        } else {
+            this.node.style.top = `${topPosition + 1}px`;
+        }
+        if (Math.abs(leftPosition - (this.targetPosition.x - 80)) < 20 && Math.abs(topPosition - (this.targetPosition.y - 20)) < 20) {
+            alert('snail says get back to work!');
+
+        }
+    }
 }
 
-//const killerSnail = new Snail(body);
-// const searchBar = document.querySelectorAll('#container');
-// searchBar.forEach((el) => {
-//     searchBar.setAttribute('style', 'border-radius:none');
-// });
+function generateStartingPosition() {
+    const startingLocations = [[-50, -50], [1000, -50], [-50, 1000], [1000, 1000]];
+    const random = Math.random();
+    if (random < 0.25) { return startingLocations[0] }
+    else if (random < 0.5) { return startingLocations[1] }
+    else if (random < 0.75) { return startingLocations[2] }
+    else { return startingLocations[3] }
+}
 
+generateStartingPosition();
 
-// const searchButton = document.querySelectorAll('#search-icon-legacy');
-// searchButton.forEach((el) => {
-//     searchButton.setAttribute('style', 'border-radius:none');
-// });
+const icon = document.getElementById('logo-icon');
+const snail = new Snail();
+icon.appendChild(snail.node);
+
+document.addEventListener('mousemove', (e) => {
+    snail.targetPosition.x = e.clientX;
+    snail.targetPosition.y = e.clientY;
+});
+
+// Move the snail continuously
+setInterval(() => {
+    snail.move();
+}, 50);
+
 
 const content = document.getElementById('content');
 
-const start = document.getElementById('start')
-const snail = new Snail(start)
-console.log(snail);
-// snail.move();
 
-const ytIcon = document.querySelectorAll('yt-icon');
-ytIcon.forEach((el) => {
-    el.remove();
-});
-
-const everything = document.querySelectorAll
-
+//BREAKS SNAIL
+//--------------
+// const ytIcon = document.querySelectorAll('yt-icon');
+// ytIcon.forEach((el) => {
+//     el.remove();
+// });
 
 const buttons = document.querySelectorAll('#buttons');
 buttons.forEach((el) => {
     el.remove();
 });
+
 const micButton = document.querySelectorAll('yt-button-shape');
 
 const breakingNews = document.querySelectorAll('ytd-rich-shelf-renderer');
@@ -75,7 +86,7 @@ breakingNews.forEach(e => {
     e.remove();
 })
 
-
+const body = document.querySelector('body');
 
 
 
@@ -92,9 +103,10 @@ const contents = document.getElementById('contents');
 console.log(contents);
 
 
-
-const body = document.querySelector('body');
-body.setAttribute('style', 'background-color:white');
+// make background black
+content.classList.add('cozy');
+const topBar = document.querySelectorAll('.style-scope ytd-masthead');
+topBar.forEach(e => e.classList.add('cozy'));
 
 const chips = document.querySelector('ytd-feed-filter-chip-bar-renderer');
 chips.remove();
@@ -109,7 +121,6 @@ divs.forEach((el) => {
 
 const logo = document.getElementById('logo');
 
-
 function adjustContent() {
     // select and remove annoying content
     const videoTitles = document.querySelectorAll('#video-title');
@@ -118,8 +129,8 @@ function adjustContent() {
     thumbnails.forEach(el => el.remove());
     images.forEach(el => el.remove());
     videoTitles.forEach((el) => {
-    // console.log('aaaaaaaaaaaaaAAAAAAAAAAAAAAA')
-    if (el.textContent) el.textContent = el.textContent.toLowerCase();
+        // console.log('aaaaaaaaaaaaaAAAAAAAAAAAAAAA')
+        if (el.textContent) el.textContent = el.textContent.toLowerCase();
     });
 
     // styling
@@ -130,26 +141,14 @@ function adjustContent() {
 
 // invoke adjust content to begin, invoke again every time mouse scroll
 adjustContent();
+
 document.addEventListener("scroll", (e) => {
     adjustContent();
 })
+
+addEventListener("mousemove", (event) => {});
 
 // remove shorts
 const shorts = document.querySelector('.style-scope ytd-rich-shelf-renderer');
 console.log(shorts);
 shorts.remove();
-
-
-// remove padding in children of contents
-
-
-
-
-// TODO: 
-//
-// change fonts
-// remove lives
-// change youtube logo
-// remove subscription logos
-// change spacing
-// allow user to keep blacklist of words, remove videos with blacklisted words
